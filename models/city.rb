b663842fcc79c0d56320667d0c9fbfab281ea0d0
awmcf <aaronmcfaull@gmail.com>
2019-02-09 16:16:33 +0000

    city and country models have CRUD functions

--- models/city.rb
+++ models/city.rb
@@ -13,17 +13,53 @@ class City
   end
 
   def save()
-      sql = "INSERT INTO cities (
-        name,
-        visited,
-        country_id )
-      VALUES
-      ( $1, $2, $3 )
-      RETURNING id"
-      values = [@name, @visited, @country_id]
-      result = SqlRunner.run(sql, values)
-      id = result.first['id']
-      @id = id
-    end
+    sql = "INSERT INTO cities (
+    name,
+    visited,
+    country_id )
+    VALUES
+    ( $1, $2, $3 )
+    RETURNING id"
+    values = [@name, @visited, @country_id]
+    result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id
+  end
+
+  def update()
+    sql = "UPDATE cities SET (name, visited, country_id) = ($1, $2, $3) WHERE id = ($4)"
+    values = [@name, @visited, @country_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM cities WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_items(country_data)
+    return city_data.map { |city| City.new(city) }
+  end
+
+  def self.all()
+    sql = "SELECT * FROM cities"
+    city_data = SqlRunner.run(sql)
+    cities = map_items(city_data)
+    return cities
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM cities
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    city = City.new(result)
+    return city
+  end
+
+  def format_name
+    return "#{@name.capitalize}"
+  end
 
 end
--- models/country.rb
+++ models/country.rb
@@ -19,4 +19,40 @@ class Country
     @id = id.to_i
   end
 
+  def update()
+    sql = "UPDATE countries SET (name) = ($1) WHERE id = ($2)"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def delete()
+    sql = "DELETE FROM countries WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.map_items(country_data)
+    return country_data.map { |country| Country.new(country) }
+  end
+
+  def self.all()
+    sql = "SELECT * FROM countries"
+    country_data = SqlRunner.run(sql)
+    countries = map_items(country_data)
+    return countries
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM countries
+    WHERE id = $1"
+    values = [id]
+    result = SqlRunner.run(sql, values).first
+    country = City.new(result)
+    return country
+  end
+
+  def format_name
+    return "#{@name.capitalize}"
+  end
+
 end
