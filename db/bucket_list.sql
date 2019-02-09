c9cb8d6e2db1b3356f6b1261a8f47f240ecb5eeb
awmcf <aaronmcfaull@gmail.com>
2019-02-09 14:44:48 +0000

    Created empty database with tables cities and countries

---
+++ db/bucket_list.sql
@@ -0,0 +1,14 @@
+DROP TABLE cities;
+DROP TABLE countries;
+
+CREATE TABLE countries (
+  id SERIAL8 primary key,
+  name VARCHAR(255)
+);
+
+CREATE TABLE cities (
+  id SERIAL8 primary key,
+  name VARCHAR(255),
+  visited BOOLEAN,
+  country_id INT8 REFERENCES countries(id)
+);
